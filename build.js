520a71d8c44614e0372a0c79c77decd17750095b
jura75 <jurabashmakov@gmail.com>
2026-09-04 16:49:52 +0300

    Update build.js

--- build.js
+++ build.js
@@ -10,3 +10,31 @@ try {
     const troopContent = fs.readFileSync(troopFile, 'utf8');
 
     const bundledCode = `// ==UserScript==
+// @name         TW Workbench
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Модульный воркбенч для TW
+// @author       Обезбашка
+// @match        https://.plemiona.pl/
+// @match        https://.voyna-plemen.ru/
+// @match        https://.tribalwars.net/
+// @grant        none
+// /UserScript
+
+/* --- Core Module --- */
+${coreContent}
+
+/* --- Troops Tab Module --- */
+${troopContent}
+`;
+
+ if (!fs.existsSync(path.join(__dirname, 'dist'))) {
+       fs.mkdirSync(path.join(__dirname, 'dist'));
+   }
+
+   fs.writeFileSync(outputFile, bundledCode, 'utf8');
+   console.log('Сборка успешно завершена!');   
+
+} catch (err) {
+console.error('Ошибка при сборке:', err);
+}
